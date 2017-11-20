20a768e8303d794abd33a73981e2a665f3bf59a5
Mushi123 <ahmed.mush@ymail.com>
2017-11-19 21:49:15 -0600

    Added post todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,28 @@
+var {mongoose}=require('./db/mongoose');
+var express=require('express');
+var bodyParser=require('body-parser');//take your json and convert into object attaching it to body of req
+
+var {Todo}=require('./models/todo');
+var {User}=require('./models/user');
+
+var app=express();
+app.use(bodyParser.json());//with this we can now send json to our express app
+app.post('/todos',(req,res) =>{
+  var todo=new Todo({
+    text:req.body.text
+  })
+  todo.save().then((doc) => {
+    res.status(201).send(doc);
+  },(err) => {
+    if(err){
+      res.status(400).send(err);
+    }
+  })
+});
+
+
+
+
+app.listen(3000,() => {
+  console.log("Server up");
+});
