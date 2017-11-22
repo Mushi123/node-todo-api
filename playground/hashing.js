c2f7080db212b4cb00e6c3fd7b09ca9ce459e54e
Mushi123 <ahmed.mush@ymail.com>
2017-11-21 19:39:34 -0600

    Hash user pw before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,35 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken')
-var data={
-  id:10,
-  access:'auth'
-};
-var token=jwt.sign(data,'123abc');//returns crytographic string
-console.log(token);
+const bcrypt=require('bcryptjs');
 
-var decoded=jwt.verify(token,'123abc')
-console.log("Decoded",decoded);
+var password='123abc!'
+// bcrypt.genSalt(10,(err,salt) => {
+//   bcrypt.hash(password,salt,(err,hash) => {
+//     console.log(hash);
+//   })
+// })
+
+var hashedPassword='$2a$10$tH7LXyYumDikSuiQYe/5tejmNukmMxTYLAIyqnbJKrIrYhQUKudAu'
+bcrypt.compare(password,hashedPassword,(err,res) => {
+  console.log(res);
+})
+
+
+
+
+
+
+// var data={
+//   id:10,
+//   access:'auth'
+// };
+// var token=jwt.sign(data,'123abc');//returns crytographic string
+// console.log(token);
+//
+// var decoded=jwt.verify(token,'123abc')
+// console.log("Decoded",decoded);
+
+
 
 // var message='I am user number 3'
 // var hash=SHA256(message).toString();
