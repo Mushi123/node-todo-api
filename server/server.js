0b72292ff180fbc15e24a5c39f9809c288d5fc3c
Mushi123 <ahmed.mush@ymail.com>
2017-11-21 23:00:12 -0600

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const bodyParser=require('body-parser');//take your json and convert into object
 const {ObjectID}=require('mongodb');
 const {Todo}=require('./models/todo');
 const {User}=require('./models/user');
+const bcrypt=require('bcryptjs')
 
 var app=express();
 const port=process.env.PORT
@@ -117,6 +118,18 @@ app.post('/users',(req,res) => {
 
 app.get('/users/me',authenticate,(req,res) => {
   res.send(req.user);
+})
+
+app.post('/users/login',(req,res) => {//dont have a token, trying to get one
+  var body=_.pick(req.body,['email','password']);
+  User.findByCredentials(body.email,body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {//using return here so that if there any errors here, the call can continue to the catch block below
+      res.header('x-auth',token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send()
+  })
+
 })
 app.listen(port,() => {
   console.log(`Server up on port ${port}`);
